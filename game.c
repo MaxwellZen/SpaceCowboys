af16e11c064c4664409471e2c5e6f26a6e5e6eb2
Maxwell Zen <maxwell.zen@gmail.com>
2022-01-15 14:17:49 -0500

    updated structure of game.c

--- game.c
+++ game.c
@@ -4,6 +4,21 @@ int main() {
     // connect to server
     int fd = ;
     while (1) {
-        //read info from server and do stuff
+        int phase = ;//read phase
+        if (phase==1) {
+
+        }
+        else if (phase==2) {
+
+        }
+        else if (phase==3) {
+            
+        }
+        else if (phase==4) {
+
+        }
+        else if (phase==5) {
+
+        }
     }
 }
